f9a50602f93d42b0b2cf916a06201c1223b70991
Sandro Thornton <sandro.thornton@gmail.com>
2025-02-22 14:46:27 -0500

    Create index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Dashboard from '../components/Dashboard.vue';
+import Presentation from '../components/Presentation.vue';
+import JoinPage from '../components/JoinPage.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Dashboard,
+    },
+    {
+      path: '/presentation/:code/:slideIndex',
+      component: Presentation,
+    },
+    {
+      path: '/join',
+      component: JoinPage,
+    },
+  ],
+});
+
+export default router;
